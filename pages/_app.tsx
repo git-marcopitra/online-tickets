ef64a538340c200f8457ffa0e4c74b2f013f1351
git-marcopitra <marcomorete73@hotmail.com>
2024-04-29 23:08:27 +0100

    :fire: chore: remove unused import

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,9 @@
 import type { AppProps } from 'next/app';
 
-import { ThemeManager } from '@/components';
-
 const App = ({ Component, pageProps }: AppProps) => (
-  <ThemeManager>
+  <>
     <Component {...pageProps} />
-  </ThemeManager>
+  </>
 );
 
 export default App;
